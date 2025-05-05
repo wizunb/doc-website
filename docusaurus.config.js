fffdf60433da27f6e9e9ffaa13ddd4c878ffb9a8
Wizzyworld <wizzyworld18@gmail.com>
2025-05-05 00:06:48 +0000

    update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -18,7 +18,7 @@ const config = {
   url: 'https://wizunb.github.io', // Your website URL
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/doc-website/',
   trailingSlash: true,
 
   // GitHub pages deployment config.
